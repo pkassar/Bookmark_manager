a04bbeebf486a5269f2b69463e447e78834672c1
Paul Kassar <paulkassar@gmail.com>
2017-05-03 10:42:50 +0100

    new page to create new link

--- app/app.rb
+++ app/app.rb
@@ -2,12 +2,18 @@ require 'sinatra/base'
 require_relative 'models/link.rb'
 
 class BookmarkManager < Sinatra::Base
-  get '/' do
-    'This is working'
-  end
 
   get '/links' do
     @links = Link.all
     erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new_link'
+  end
+
+  post '/links/saved' do
+    Link.create(title: params[:title], url: params[:url])
+    redirect '/links'
   end
 end
---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,8 @@
+feature 'Form for new links' do
+  scenario 'click button to start creating a new link' do
+    visit '/links/new'
+    fill_in('title', with: 'makers' )
+    fill_in('url', with: 'makersacademy.com' )
+    click_button('new link')
+  end
+end
