8e0c935c1400ab15af860846754ca1bc7e8d9fcf
Paul Kassar <paulkassar@gmail.com>
2017-05-04 15:23:34 +0100

    changed to .include for the tags

--- app/app.rb
+++ app/app.rb
@@ -24,7 +24,7 @@ class BookmarkManager < Sinatra::Base
 
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
+    @links.include? tag ? tag.links : []
     erb :'links/index'
   end
 end
